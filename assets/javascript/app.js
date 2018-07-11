b4f6fe72a408d202f2193c0bd53fbcd040621b4d
Brandon Weir <brandon.weir@alumni.utoronto.ca>
2018-07-11 19:24:51 -0400

    added more questions, corrected some functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,149 +1,232 @@
-var questions = [
-  "In 'The Matrix', what was the name of Morpheus' ship?",
-  "What is the model number of Arnold's Schwarzenegger's terminator in 'Terminator 2'?",
-  "Who is NOT one of the original six Avengers?",
-  "Which animal's DNA was used to clone the dinosaurs in 'Jurassic Park'?"
-];
-var answers = ["The Nebuchadnezzar", "T-800", "Spiderman", "Frog"];
-var firstChoice = ["The Vigilant", "T-3000", "Spiderman", "Frog"];
-var secondChoice = ["The Logos", "T-1000", "Iron Man", "Mosquito"];
-var thirdChoice = ["The Merovingian", "T-OK715", "Black Widow", "Bird"];
-var fourthChoice = ["The Nebuchadnezzar", "T-800", "Hawkeye", "Lizard"];
-var count = 0;
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var images = [
-  "assets/images/bulletTime.gif",
-  "assets/images/t800.gif",
-  "assets/images/avengers.gif",
-  "assets/images/jurassicpark.gif"
-];
+$(document).ready(function() {
+  var questions = [
+    "In 'The Matrix', what was the name of Morpheus' ship?",
+    "What is the model number of Arnold's Schwarzenegger's terminator in 'Terminator 2'?",
+    "Who is NOT one of the original six Avengers?",
+    "Which animal's DNA was used to clone the dinosaurs in 'Jurassic Park'?",
+    "In 'Aliens', how long was Ripley in hypersleep before being found?",
+    "Which character in 'Star Wars: The Last Jedi' said 'It's time for the Jedi to end.'?",
+    "Where did Wonder Woman rescue hostages at the beginning of 'Justice League'?",
+    "In 'Back to the Future', how fast did the Delorean need to go in order to time travel?"
+  ];
+  var answers = [
+    "The Nebuchadnezzar",
+    "T-800",
+    "Spiderman",
+    "Frog",
+    "57 years",
+    "Luke Skywalker",
+    "A bank",
+    "88 mph"
+  ];
+  var firstChoice = [
+    "The Vigilant",
+    "T-3000",
+    "Spiderman",
+    "Frog",
+    "98 years",
+    "Rey",
+    "A mall",
+    "73 parsecs"
+  ];
+  var secondChoice = [
+    "The Logos",
+    "T-1000",
+    "Iron Man",
+    "Mosquito",
+    "32 lightyears",
+    "Luke Skywalker",
+    "A museum",
+    "141.62 kmh"
+  ];
+  var thirdChoice = [
+    "The Merovingian",
+    "T-OK715",
+    "Black Widow",
+    "Bird",
+    "57 years",
+    "Supreme Leader Snoke",
+    "An office building",
+    "88 mph"
+  ];
+  var fourthChoice = [
+    "The Nebuchadnezzar",
+    "T-800",
+    "Hawkeye",
+    "Lizard",
+    "200 lightyears",
+    "Kylo Ren",
+    "A bank",
+    "49 cycles"
+  ];
+  var count = 0;
+  var correct = 0;
+  var incorrect = 0;
+  var unanswered = 0;
+  var images = [
+    "assets/images/bulletTime.gif",
+    "assets/images/t800.gif",
+    "assets/images/avengers.gif",
+    "assets/images/jurassicPark.gif",
+    "assets/images/ripley.gif",
+    "assets/images/luke.gif",
+    "assets/images/wonderWoman.gif",
+    "assets/images/DeLorean.gif"
+  ];
 
-var number = 30;
+  var number = 30;
 
-//  Variable that will hold our interval ID when we execute
-//  the "run" function
-var intervalId;
+  //  Variable that will hold our interval ID when we execute
+  //  the "run" function
+  var intervalId;
 
-function startGame() {
-  $("button").remove();
+  //function that starts the game
+  function startGame() {
+    $("button").remove();
 
-  $("#timeRemaining").html("<div>Time Remaining: " + number + "</div><br>");
-
-  startTimer();
-
-  function startTimer() {
-    intervalId = setInterval(decrement, 1000);
-  }
-
-  function decrement() {
-    number--;
     $("#timeRemaining").html("<div>Time Remaining: " + number + "</div><br>");
-    if (number === 0) {
-      stopTimer();
-      $("#timeRemaining").html("<div>Time's Up!<div>");
-      $("#timeRemaining").append(
-        "<div>The correct answer was: " + answers[count] + "</div>"
-      );
-      $("#questions").remove();
-      $(".choices").remove();
-      displayImage();
-      unanswered++;
-      count++;
+
+    startTimer();
+
+    displayQuestion();
+
+    function startTimer() {
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+      number--;
+      $("#timeRemaining").html("<div>Time Remaining: " + number + "</div><br>");
+
+      //if the time runs out, then let the user know and display the correct answer along with the image, increase the unanswered question count
+      //and move on to the next question
+      if (number === 0) {
+        stopTimer();
+        $("#timeRemaining").html("<div>Time's Up!<div>");
+        $("#timeRemaining").append(
+          "<div>The correct answer was: " + answers[count] + "</div>"
+        );
+        $("#questions").text("");
+        $(".choices").text("");
+        displayImage();
+        unanswered++;
+        count++;
+      }
+    }
+
+    //stops the timer and displays the next question
+    function stopTimer() {
+      clearInterval(intervalId);
+      number = 31;
+      if (count < questions.length - 1) {
+        setTimeout(nextQuestion, 5000);
+        setTimeout(startTimer, 5000);
+      }
+    }
+
+    //makes the quiz options 'clickable' and runs the solution function to check the user's answer
+    $("#firstChoice").on("click", solution);
+    $("#secondChoice").on("click", solution);
+    $("#thirdChoice").on("click", solution);
+    $("#fourthChoice").on("click", solution);
+
+    //checks the user's selection against the answer in the answer array
+    function solution() {
+      //if the user's selection is correct, then stop time, display image, increase correct count, and move to next question
+      if ($(this).text() === answers[count]) {
+        stopTimer();
+        correctAnswer();
+        displayImage();
+        correct++;
+        count++;
+      } else {
+        //if incorrect, then stop time, display correct answer, display image, increase incorrect count, and move to next question
+        stopTimer();
+        incorrectAnswer();
+        displayImage();
+        incorrect++;
+        count++;
+      }
     }
   }
 
-  function stopTimer() {
-    clearInterval(intervalId);
+  //displays a related image to the user
+  function displayImage() {
+    $("#image-holder").html("<img src=" + images[count] + " />");
   }
 
-  displayQuestion();
-
-  $("#firstChoice").on("click", solution);
-  $("#secondChoice").on("click", solution);
-  $("#thirdChoice").on("click", solution);
-  $("#fourthChoice").on("click", solution);
-
-  //use the code below to create a checkAnswer function that compares the user selection to the answer
-  //and increases/decreases the win count, and the count for the index
-
-  function solution() {
-    if ($(this).text() === answers[count]) {
-      stopTimer();
-      correctAnswer();
-      displayImage();
-      correct++;
-      count++;
-      displayQuestion();
-    } else {
-      stopTimer();
-      incorrectAnswer();
-      displayImage();
-      incorrect++;
-      count++;
-      displayQuestion();
-    }
+  //confirms to the user that they've answered correctly
+  function correctAnswer() {
+    $("#timeRemaining").append("<div>Correct!</div>");
+    $("#questions").text("");
+    $(".choices").text("");
   }
 
-  // userChoice = $(".choices").on("click", function() {
-  //   if (userChoice === answers[0]) {
-  //     correct++;
-  //     alert("You got that right!");
-  //   } else {
-  //     incorrect++;
-  //     alert("You got that wrong!");
-  //   }
-  // });
-}
-
-//function displayImage() {
-//   $("#image").html("<img src=" + images[count] + " width='400px'>");
-// }
-//function displayAnswer(){
-//clearInterval (setInterval);
-
-function displayImage() {
-  $("#image-holder").html("<img src=" + images[count] + " alt='Bullet Time'/>");
-}
-
-function correctAnswer() {
-  $("#timeRemaining").append("<div>Correct!</div>");
-  $("#questions").remove();
-  $(".choices").remove();
-}
-
-function incorrectAnswer() {
-  $("#timeRemaining").append(
-    "<div>Incorrect! The correct answer was: " + answers[count] + "</div>"
-  );
-  $("#questions").remove();
-  $(".choices").remove();
-}
-
-function displayQuestion() {
-  $("#questions").text(questions[count]);
-  $("#firstChoice").text(firstChoice[count]);
-  $("#secondChoice").text(secondChoice[count]);
-  $("#thirdChoice").text(thirdChoice[count]);
-  $("#fourthChoice").text(fourthChoice[count]);
-}
-
-function endGame() {
-  if (count === questions.length - 1) {
-    stopTimer();
-    $("#questions").remove();
-    $(".choices").html(
-      "<div>The game is now finished. Here are your results:</div>"
+  //shows the user what the correct answer is after they answer incorrectly
+  function incorrectAnswer() {
+    $("#timeRemaining").append(
+      "<div>Incorrect! The correct answer was: " + answers[count] + "</div>"
     );
-    $(".choices").append("<div>Correct Answers:" + correct + "</div>");
-    $(".choices").append("<div>Incorrect Answers:" + incorrect + "</div>");
-    $(".choices").append("<div>Unanswered Answers:" + unanswered + "</div>");
+    $("#questions").text("");
+    $(".choices").text("");
   }
-}
 
-// stopTime;
+  //displays the questions to the user
+  function displayQuestion() {
+    $("#questions").html("<div>" + questions[count] + "</div>");
+    $("#firstChoice").html("<div>" + firstChoice[count] + "</div>");
+    $("#secondChoice").html("<div>" + secondChoice[count] + "</div>");
+    $("#thirdChoice").html("<div>" + thirdChoice[count] + "</div>");
+    $("#fourthChoice").html("<div>" + fourthChoice[count] + "</div>");
+  }
 
-$("#startButton").on("click", function() {
-  startGame();
+  function nextQuestion() {
+    $("#image-holder").text("");
+    if (count === questions.length - 1) {
+      endGame();
+    } else {
+      displayQuestion();
+    }
+  }
+
+  //checks to see if the game is out of questions to ask
+  function endGame() {
+    stopTimer();
+    //if the game is out of questions, then display the user's results
+    if (count === questions.length) {
+      // stopTimer();
+      $("#questions").html(
+        "<div>The game is now finished. Here are your results:</div>"
+      );
+      $("#firstChoice").text("<div>Correct Answers:" + correct + "</div>");
+      $("#secondChoice").text("<div>Incorrect Answers:" + incorrect + "</div>");
+      $("#thirdChoice").text(
+        "<div>Unanswered Answers:" + unanswered + "</div>"
+      );
+      $("#fourthChoice").text("");
+      $("#startButton").html("<button>Start Over?</button>");
+
+      $("#startButton").on("click", function() {
+        reset();
+      });
+    }
+  }
+
+  //resets the game results
+  function reset() {
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+    count = 0;
+    startGame();
+  }
+
+  //when the user clicks the Start button, then the game starts
+  $("#startButton").on("click", function() {
+    startGame();
+  });
 });
+
+//instead of removing the questions and choices div entirely, just replace them with empty strings
+//OR toggle their css classes; .addClass or .removeClass and set the CSS property to none
+//pick whichever works best
